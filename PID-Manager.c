b8c04de8521c6d741a6c130bad6a5a2466069bfc
leosaradd100 <leosarad100@gmail.com>
2019-03-21 20:58:26 +0530

    Menu functions and allocateMap() inittialized

--- PID-Manager.c
+++ PID-Manager.c
@@ -20,6 +20,9 @@ int totalAvail();
 int nextAvail();
 // Menu Functions
 void menuA();
+void menuB();
+void choiceB();
+
 
 // Main Function
 int main(){
@@ -39,11 +42,56 @@ void menuA(){
 	ch=getchar();
 	if(ch=='1'){
 		allocateMap();
+		menuB();
 	} else{ 
 		printf("\nProgram Ends\n");
 		exit(0);
 	}
 }
+
+void menuB(){
+   printf("\nMenu:");
+	printf("\n1. Total PID's available");
+	printf("\n2. First PID available");
+	printf("\n3. Create Processes");
+	printf("\n4. Press any key to Exit.");
+	choiceB();
+}
+
+void choiceB(){
+	char ch;
+	printf("\n\nMenu Choice (|1|2|3|): ");
+	getchar();
+	ch=getchar();
+	if(ch=='1'){
+		int totalId=totalAvail();
+		printf("\nTotal Available PID: %d",totalId);
+		choiceB();
+	} else if(ch=='2'){
+		int nextId=nextAvail();
+		printf("\nFist Available PID: %d",nextId);
+		choiceB();
+	} else if(ch=='3'){
+		int num,time;
+		printf("\nCreate Process:\nInput-> Number of Process | Execution Time :");
+		scanf("%d %d",&num,&time);
+		int valid=totalAvail();
+		if(num>0 && num<=valid && time>0){
+			printf("\nProcesses created: ");
+			for(int i=0;i<num;i++){
+				createProcess(i,time);
+			}
+		} else{
+			printf("\nInputs Invalid)");
+		}
+		choiceB();
+	}
+	else{ 
+		printf("Program Ends\n");
+		exit(0);
+	}
+}
+
 int allocateMap(){
 	for(int i=0;i<size;i++){
 		bitMap[i]=0;
@@ -56,7 +104,8 @@ int totalAvail(){
 }
 int nextAvail(){
 }
-void *createProcess(){
+void *createProcess(int num,int time){
+	printf("\nProcess PID: %d\nExecution time: %d",num,time);
 }
 int allocatePID(){
 }
